9671feeed7b6c35539bef644155b2118134a7391
Raquel Leão <vc.raquel@gmail.com>
2021-11-24 15:34:36 -0300

    criar documento data com as adicoes para o banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+--         dados que serão inseridos no banco de dados quando a aplicação for reinicializada
+          INSERT INTO CATEGORIAS (nome) VALUES ('Praia');
+          INSERT INTO CATEGORIAS (nome) VALUES ('Montanha');
+          INSERT INTO CATEGORIAS (nome) VALUES ('Campo');
+          INSERT INTO CATEGORIAS (nome) VALUES ('Floresta');
+          INSERT INTO CATEGORIAS (nome) VALUES ('Historico');
+
+          INSERT INTO REGIOES (nome) VALUES ('Nordeste');
+          INSERT INTO REGIOES (nome) VALUES ('Norte');
+          INSERT INTO REGIOES (nome) VALUES ('Sudeste');
+          INSERT INTO REGIOES (nome) VALUES ('Sul');
+          INSERT INTO REGIOES (nome) VALUES ('Centro-Oeste');
+
+
