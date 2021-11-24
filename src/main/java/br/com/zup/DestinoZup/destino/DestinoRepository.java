144ed9d3595c646e914728930ab16d267b2d831b
Raquel Leão <vc.raquel@gmail.com>
2021-11-24 15:36:40 -0300

    corrigir repositorio para Destino

--- src/main/java/br/com/zup/DestinoZup/destino/DestinoRepository.java
+++ src/main/java/br/com/zup/DestinoZup/destino/DestinoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 //não é necessário @Repository,
 // normalmente essa anotação é usada quando a repository é uma classe (não uma interface)
 // e você implementa a crud(e não extends)
-public interface DestinoRepository extends CrudRepository {
+public interface DestinoRepository extends CrudRepository <Destino, Integer>{
 
 }
