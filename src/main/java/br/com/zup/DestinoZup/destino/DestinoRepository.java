5a58c7dd9fb6f110f8993319d8278c584bfbf330
Raquel Leão <vc.raquel@gmail.com>
2021-11-24 15:35:58 -0300

    criar repositorio para Destino

--- src/main/java/br/com/zup/DestinoZup/destino/DestinoRepository.java
+++ src/main/java/br/com/zup/DestinoZup/destino/DestinoRepository.java
@@ -1,2 +1,9 @@
-package br.com.zup.DestinoZup.destino;public interface DestinoRepository {
+package br.com.zup.DestinoZup.destino;
+
+import org.springframework.data.repository.CrudRepository;
+//não é necessário @Repository,
+// normalmente essa anotação é usada quando a repository é uma classe (não uma interface)
+// e você implementa a crud(e não extends)
+public interface DestinoRepository extends CrudRepository {
+
 }
